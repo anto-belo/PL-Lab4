9eb9d34bb32431aa33c703b256bc4b89f5b3a4b4
anto-belo <70717945+anto-belo@users.noreply.github.com>
2020-09-09 12:15:14 +0300

    UPD

--- main.c
+++ main.c
@@ -4,45 +4,29 @@
 typedef struct {
     int number;
     char type;
-    char *destination;
-    char *departure_time;
+    char destination[20];
+    char departure_time[6];
 } flight;
 
 int main() {
-    flight flights[5];
-
-    flights[0].number = 2341;
-    flights[0].type = 'A';
-    flights[0].destination = "Minsk";
-    flights[0].departure_time = "12:20";
-
-    flights[1].number = 2231;
-    flights[1].type = 'B';
-    flights[1].destination = "Moscow";
-    flights[1].departure_time = "13:40";
-
-    flights[2].number = 2111;
-    flights[2].type = 'B';
-    flights[2].destination = "Ankara";
-    flights[2].departure_time = "18:10";
-
-    flights[3].number = 4532;
-    flights[3].type = 'A';
-    flights[3].destination = "Minsk";
-    flights[3].departure_time = "03:05";
-
-    flights[4].number = 4632;
-    flights[4].type = 'C';
-    flights[4].destination = "Minsk";
-    flights[4].departure_time = "11:30";
+    flight flights[] = { 9341, 'A', "Minsk", "12:20",
+                         6233, 'B', "Ankara", "13:20",
+                         5454, 'A', "Moscow", "13:20",
+                         3239, 'A', "Minsk", "13:20",
+                         2435, 'C', "Berlin", "13:20",
+                         4237, 'D', "London", "13:20",
+                         2573, 'D', "Paris", "13:20",
+                         1235, 'A', "Barcelona", "13:20",
+                         5246, 'B', "Minsk", "13:20"};
 
     char query[30];
-    puts("Input string: ");
+    puts("Input destination city: ");
     gets(query);
 
-    for (int i = 0; i < 5; i++) {
+    for (int i = 0; i < (sizeof(flights) / sizeof(flight)); i++) {
         if (strcmp(flights[i].destination, query) != 0) continue;
-        printf("%s (%c): #%d - %s\n", flights[i].destination, flights[i].type, flights[i].number, flights[i].departure_time);
+        printf("%s (%c): #%d - %s", flights[i].destination, flights[i].type, flights[i].number, flights[i].departure_time);
+        printf("\n");
     }
     return 0;
 
